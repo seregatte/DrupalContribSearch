05e9bcac6e02c06a68a4961196c36ca63a82cfaa
João Paulo Seregatte Costa <seregatte@gmail.com>
2012-05-14 10:44:48 -0300

    Primeiro Commit

---
+++ drupal_contrib_search.py
@@ -0,0 +1,25 @@
+import webbrowser
+import sublime, sublime_plugin
+
+class DrupalContribSearchCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+
+        selection = ""
+        for region in self.view.sel():
+            selection += self.view.substr(region)
+
+        webbrowser.open("http://drupalcontrib.org/api/search/7/%s" % selection)
+
+    def is_visible(self):
+
+        is_visible = False
+
+        for region in self.view.sel():
+            if not region.empty():
+                is_visible = True
+
+        return is_visible
+
+    def is_enabled(self):
+        return self.is_visible()
